85238de35b29d60ce8a93b4f4127544b81730ab8
Hendrik Hofstadt <dev@slamper.me>
2016-03-27 17:00:15 +0100

    Optimized script

--- grow.sh
+++ grow.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
-./growpart /dev/vda 1
-partprobe
-resize2fs /dev/vda1
+RESIZE_DEV=${RESIZE_DEV:?"RESIZE_DEV not set."}
+
+if [ -b "${RESIZE_DEV}" ]; then
+  ./growpart ${RESIZE_DEV} 1
+  partprobe
+  resize2fs ${RESIZE_DEV}1
+else
+  echo "Block device expected: ${RESIZE_DEV} is not."
+  exit 1
+fi
