bd0d22cc9921f683aa096c547d655e2040ecd0f4
Rob Nelson <guruvan@users.noreply.github.com>
2016-04-08 13:45:47 -0700

    Add usage instructions on failure

--- grow.sh
+++ grow.sh
@@ -2,7 +2,11 @@
 RESIZE_DEV="$1"
 test -b /dev/${RESIZE_DEV} || export RESIZE_DEV=""
 if [ "X${RESIZE_DEV}" = "X" ] ; then
-   echo "Must set RESIZE_DEV=[/dev/a_valid_block_device]"
+   echo "Must set RESIZE_DEV=[a_valid_block_device]"
+   echo "docker run -it --rm guruvan/resizefs xvda"
+   echo "or"
+   echo "docker run -it --rm --privileged guruvan/resizefs xvda1"
+   echo "or any other available valid block device"
    exit 1
 elif [ "${RESIZE_DEV}" = "xvda1" ] ; then
    resize2fs /dev/${RESIZE_DEV}
